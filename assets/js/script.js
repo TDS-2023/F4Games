cb8b42aa96a1fbe488ec4168feb3f47e3f730fca
jaopedroserrano <jooaoopedrooserrano@gmail.com>
2023-10-04 14:37:13 +0044

    jogos ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,20 @@ document.addEventListener("DOMContentLoaded", function () {
     setInterval(nextImage, 3000);
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    var btn = document.querySelector('#show-or-hide');
+    var container = document.querySelector('.container');
+
+    btn.addEventListener('click', function () {
+        if (container.style.display === 'block' || container.style.display === '') {
+            container.style.display = 'none';
+        } else {
+            container.style.display = 'block';
+            container.style.position = 'fixed';
+            container.style.top = '40%';
+            container.style.left = '80%';
+            container.style.transform = 'translate(-50%, -50%)';
+        }
+    });
+});
+
